163d261161c398efc69eda0a46854791985e0f85
Durgesh Kumar <durgeshkumar.dk570@gmail.com>
2020-08-29 20:21:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Dashboard from './Components/Dashboard/dashboard'
+import Mainpage from './Components/Mainpage/mainpage'
+import generalPlotMap from './Components/General_Plot/generalPlotMap'
+import particularPlotMap from './Components/Particular_Plot/paticularPlotMap';
+import history from './Components/History/history'
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path='/' component={Mainpage} />
+        <Route path='/dashboard' component={Dashboard} />
+        <Route path='/general' component={generalPlotMap} />
+        <Route path='/track_by_id' component={particularPlotMap} /> 
+        <Route path='/history' component={history} />
+      </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
